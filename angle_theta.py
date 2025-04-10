e8fb30d72da8efc280ffb4078b1841b3cabf68d9
2100 <149405264+C-for-coursework@users.noreply.github.com>
2025-04-10 23:49:26 +0800

    Update angle_theta.py with comments

--- angle_theta.py
+++ angle_theta.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 def angle_theta(a, b):
+    '''Calculate the angle between two vectors'''
     a = np.array(a)
     b = np.array(b)
+    
     # Dot product
     dot_product = np.dot(a, b)
 
     norm_a = np.linalg.norm(a)
     norm_b = np.linalg.norm(b)
-    # Angle
+    
     cos_theta = dot_product / (norm_a * norm_b)
     cos_theta = np.clip(cos_theta, -1.0, 1.0)  # Float error
     return np.degrees(np.arccos(cos_theta))
